232e46cd966206a8ad9d98e133ba4fcc3227479f
Blackhole0S <smallbike@protonmail.com>
2025-05-24 08:37:15 +0200

    Update service.h

--- include/service.h
+++ include/service.h
@@ -1,1 +1,12 @@
+// include/service.h
 
+#ifndef SERVICE_H
+#define SERVICE_H
+
+int service_add(const char *name, const char *exec_path);
+int service_start(const char *name);
+int service_stop(const char *name);
+int service_restart(const char *name);
+void service_cleanup(void);
+
+#endif // SERVICE_H
