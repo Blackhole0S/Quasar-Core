cb27074ee620240520ac11923480285bc1c85674
Blackhole0S <smallbike@protonmail.com>
2025-05-24 08:39:43 +0200

    Update logging.h

--- include/logging.h
+++ include/logging.h
@@ -1,1 +1,20 @@
+// include/logging.h
 
+#ifndef LOGGING_H
+#define LOGGING_H
+
+typedef enum {
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARNING,
+    LOG_LEVEL_ERROR
+} LogLevel;
+
+void logging_init(const char *ident);
+void log_message(LogLevel level, const char *fmt, ...);
+
+// Convenience macros
+#define log_info(...) log_message(LOG_LEVEL_INFO, __VA_ARGS__)
+#define log_warning(...) log_message(LOG_LEVEL_WARNING, __VA_ARGS__)
+#define log_error(...) log_message(LOG_LEVEL_ERROR, __VA_ARGS__)
+
+#endif // LOGGING_H
